5fee6e526cf6759c019304be7fb4d5a82a29056f
HyunwooPark-dev <HyunwooPark-dev@users.noreply.github.com>
2026-02-09 09:28:52 +0000

    deploy: a3fd7c68119a80b33f5ac6da1ecf6fd35da1fac1

--- assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "GitHub Pages로 Engineering Log 띄우기 삽질기",
-        "excerpt":"최초 Engineering Log 작성글  ","categories": ["devlog"],
-        "tags": [],
-        "url": "/HyunwooPark.github.io/devlog/2026/02/09/github-pages-setup-log.html",
-        "teaser": null
-      }]
