88ee671d8a96f026c39ca547dddbac3a17fd1b9c
hwp-log <hwp-log@users.noreply.github.com>
2026-02-11 15:09:44 +0000

    deploy: 861fb0bdfab04cd980d3d20fa18727dc7bf01478

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "GitHub Pages로 Engineering Log 띄우기 삽질기",
-        "excerpt":"최초 Engineering Log 작성글  ","categories": ["devlog"],
-        "tags": [],
-        "url": "/devlog/2026/02/09/github-pages-setup-log.html",
-        "teaser": null
-      },{
         "title": "[next.js] x-com의 ActiveLink 만들기",
         "excerpt":"next.js 기본기를 다지고자 이전 정리한 글에 이어 작성하는 기록이다. → 이전 정리 노션 문서 바로가기 강의내용은 x.com을 클론코딩하는 내용으로, 서비스에서 사용되는 기능을 next.js로 구현하는 것이 목적이다. 현재까지 구현한 내용은 x.com의 메인페이지 및 로그인페이지를 구현하였고 Home메뉴의 레이아웃을 작성한 상태이다. 어쨌든 중요한 것은.. 이걸 만들면서도 정작 진짜 필요한 기술인지에 대한 의문이 들었어야...","categories": ["next.js"],
         "tags": [],
@@ -15,5 +9,11 @@ var store = [{
         "excerpt":"글 작성 목적 next.js 기본기를 다지고자 이전 정리한 글에 이어 작성하는 기록이다. → 이전 글 바로가기 x-com의 좌측메뉴 작성에 이어, 이번에는 우측메뉴를 작성하려고 한다. 우측 메뉴는 사진과 같이 “나를 위한 트렌드” 메뉴가 존재한다. 우측에 보면 Today’s News, What’s happening등 구 트위터의 “나를 위한 트렌드” 자리이다. 이 부분을 오늘 구현 해...","categories": ["next.js"],
         "tags": [],
         "url": "/next.js/2026/02/11/next.js-session-2-3.html",
+        "teaser": null
+      },{
+        "title": "[next.js] Context API - TabProvider를 활용한 홈 탭 상태관리",
+        "excerpt":"x.com의 로그인 후, 메인페이지를 보게 된다면 중앙에 ‘추천/팔로우’와 ‘내용’이 등장한다. 이때 추천/팔로우를 번갈아서 클릭 했을 때, 내용이 전환 될 필요가 존재한다. 문제는 ‘추천/팔로우’와’내용’의 각 컴포넌트가 형제(병렬) 관계이기 때문에, 일반적인 props 전달로는 상태를 공유 할 수 없다는 점이다.(React는 “데이터는 위에서 아래로 흐른다.” 라는 단방향 데이터흐름 설계원칙을 가지고 있어 부모 → 자식...","categories": ["next.js"],
+        "tags": [],
+        "url": "/next.js/2026/02/11/next.js-session-2-4.html",
         "teaser": null
       }]
